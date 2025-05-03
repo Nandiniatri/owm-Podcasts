62b41fd2387dcc4a50d0196ad0a8eaed03ba90dc
Nandiniatri <atrinandini8@gmail.com>
2025-05-03 12:58:28 +0530

    pushed updated code

---
+++ src/fixtures/guestCuratorCard.jsx
@@ -0,0 +1,11 @@
+export default [
+    {
+        id:1,
+        image:"https://static.pocketcasts.com/share/images/942c7c15-46e0-45a5-86b1-4a49e12f8479-author.jpg",
+        title:"Guest curator",
+        linkTitle:"See List",
+        subTitle:"Tal Minear",
+        content:"Tal Minear is a voice actor, sound designer, and audio drama producer. They're the creator of Re: Dracula, Sidequesting, and several other productions that can be found hiding under rugs and around corners.",
+        guestCurImg:"https://static.pocketcasts.com/share/images/942c7c15-46e0-45a5-86b1-4a49e12f8479-collage-web.png"
+    }
+]
