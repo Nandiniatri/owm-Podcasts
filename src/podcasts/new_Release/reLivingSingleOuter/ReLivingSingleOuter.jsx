5d9a6d06ba283521b142319e383fc72498bf8dac
Nandiniatri <atrinandini8@gmail.com>
2025-05-05 22:50:58 +0530

    pushed updated code

--- src/podcasts/new_Release/reLivingSingleOuter/ReLivingSingleOuter.jsx
+++ src/podcasts/new_Release/reLivingSingleOuter/ReLivingSingleOuter.jsx
@@ -1,9 +1,31 @@
-import { useContext } from "react";
+import { useContext , useState} from "react";
 import { AppContext } from "../../../contextApi/AppContext";
 import '../commonCss.css';
+import Modal from "../../../component/modal/Modal";
+import YouTube from "react-youtube";
+import Button from "../../../component/Button";
+
+const opts = {
+    width: '100%',
+    height: '100%',
+    playerVars: {
+        autoplay: 1,
+    },
+};
 
 const ReLivingSingleOUTER = () => {
-    const { reLivingSingleOUTER } = useContext(AppContext);
+    const { reLivingSingleOUTER, showData, setShowData } = useContext(AppContext);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const handleOpenModal = (podcast) => {
+        // console.log(podcast);
+        setIsModalOpen(true);
+        setShowData([podcast]);
+    }
+    const handleCloseModal = () => {
+        setIsModalOpen(false);
+    }
+
 
     return (
         <div className="section-container">
@@ -17,13 +39,30 @@ const ReLivingSingleOUTER = () => {
 
             <div className="card-grid">
                 {reLivingSingleOUTER.map((podcast) => (
-                    <div className="card" key={podcast.id}>
+                    <div className="card" key={podcast.id} onClick={() => handleOpenModal(podcast)}>
                         <img src={podcast.image} alt={podcast.title} className="card-img" />
                         <h3 className="card-title">{podcast.title}</h3>
                         <p className="card-subtitle">{podcast.content}</p>
                     </div>
                 ))}
             </div>
+
+            <div>
+                <Modal isOpen={isModalOpen} isClosed={handleCloseModal}>
+                    <div className="modal-btn-div">
+                        <Button onClick={handleCloseModal} className="modal-close-button">Close</Button>
+                    </div>
+                    <div>
+                        {showData.map((item) => (
+                            <div className="youtube-video" key={item.id}>
+                                <YouTube videoId={item.videoUrlId} className="youtube-y" opts={opts} />
+                            </div>
+                        ))}
+                    </div>
+                </Modal>
+            </div>
+
+
         </div>
     );
 };
