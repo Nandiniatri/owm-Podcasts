9738d9ffe4dc92d6996467b3c3a4eb00eb68f6b1
Nandiniatri <atrinandini8@gmail.com>
2025-05-05 12:30:10 +0530

    pushed updated code

---
+++ src/fixtures/NewRelease/ranveerallahbadia/RanveerAllahbadia.jsx
@@ -0,0 +1,9 @@
+export default [
+    { id: 1, image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQu5pBXuyorIq7AOO2EYlipDWHEyO94T-VW6w&s", title: "Masters' Union Dr. Nandini Seth", content: "2025's Most Important Career Podcast - AI Skills For All Ages", videoUrlId: "YG1sW00jwLY", youtubeUrl: "https://youtu.be/YG1sW00jwLY?si=XB86x1F3rTzqo_iN" },
+
+    { id: 2, image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS3kz51OEpRWyBGSHAVQYhsMCrlSxEtI-i-0A&s", title: "C Sivasankaran", content: "UNBEATABLE Mindset: From a ₹7000 crore loss to a powerful comeback — inspiring stories of Ratan Tata and others.", videoUrlId: "OLmate4r6bU", youtubeUrl: "https://youtu.be/OLmate4r6bU?si=RLcM08ka2rbVz0OQ" },
+
+    { id: 3, image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuIznG4c1ddR3mbhFf5yq0tMUvP5KTTn36Ig&s", title: "Prathamesh Sinha", content: "TRS Kids Special - Meet India's Youngest Inspiration Prathamesh Sinha", videoUrlId: "gO-1O_C5Fms", youtubeUrl: "https://youtu.be/gO-1O_C5Fms?si=K3qxStxaqO9PUhyu" },
+
+    { id: 4, image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS3MDzCWlVZgm92ftF4M0KLeKVCSYcPJn4OPw&s", title: "Major Prajakta Desa", content: "Kashmir's ASLI Story - PAK Army, Pahalgam Attack & India's Retaliation", videoUrlId: "tx6l9NNGCBc", youtubeUrl: "https://youtu.be/tx6l9NNGCBc?si=9YhEIxbmHciCt9dB" },
+]
