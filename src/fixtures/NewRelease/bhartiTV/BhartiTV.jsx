7c79750fe1470e8e99407c51f9f7bdd8a8764db2
Nandiniatri <atrinandini8@gmail.com>
2025-05-05 12:45:28 +0530

    pushed updated code

---
+++ src/fixtures/NewRelease/bhartiTV/BhartiTV.jsx
@@ -0,0 +1,9 @@
+export default [
+    { id: 1, image: "https://i.ytimg.com/vi/K4ea2gzQclU/hqdefault.jpg?v=6814e489", title: "Geeta Kapur", content: "Geeta Kapur: The Real Story", videoUrlId: "K4ea2gzQclU", youtubeUrl: "https://youtu.be/K4ea2gzQclU?si=tCv3r9czLadgZzWO" },
+
+    { id: 2, image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVDEem717IXDocnTrRLJxUdwubBStXNRE6Cg&s", title: "Vipul Amrutlal Shah", content: "Vipul Amrutlal Shah - The Man Behind 'The Kerala Story' ", videoUrlId: "qcO5Ra5TjkE", youtubeUrl: "https://youtu.be/qcO5Ra5TjkE?si=XkeRmy6HvENKIMoE" },
+
+    { id: 3, image: "https://i.ytimg.com/vi/iWMOg-6Wzzg/maxresdefault.jpg", title: "Acharya Sakshi & Acharya Sanjeev", content: "Acharya Sakshi & Acharya Sanjeev On Finances , Wealth & Prosperity", videoUrlId: "iWMOg-6Wzzg", youtubeUrl: "https://youtu.be/iWMOg-6Wzzg?si=etS179KxxKIyhHHT" },
+
+    { id: 4, image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSrzxeLiulTxb4WlYYcnINnAKQO1Rmlv0jBfg&s", title: "Divyanka Tripathi & Vivek Dahiya", content: "Divyanka Tripathi & Vivek Dahiya : Stories Unwrapped", videoUrlId: "MsGD8i77flc", youtubeUrl: "https://youtu.be/MsGD8i77flc?si=mOHKLmP2IeHozvgb" }
+]
