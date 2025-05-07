e85765ecba55eac564959ff01f992b38d51a991e
Nandiniatri <atrinandini8@gmail.com>
2025-05-07 12:33:52 +0530

    pushed updated code

--- src/podcasts/new_Release/NewRelease.jsx
+++ src/podcasts/new_Release/NewRelease.jsx
@@ -7,11 +7,12 @@ import ReLivingSingleOUTER from "./reLivingSingleOuter/ReLivingSingleOuter";
 const NewRelease = () => {
     return (
         <div className="new-release-main-container">
-            <div>
-                <h1>New Release</h1>
+            <div className="new-release-div1-h1-div">
+                <h1>New Release</h1> 
             </div>
 
             <div>
+                
                 <RajShamani />
                 <BhartiTvOuter />
                 <RanveerAllahbadiaOUTER />
